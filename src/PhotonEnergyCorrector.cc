8181fc8493dd61f6bd5e60d3f0c1e52a11bbf62b
cmsbuild <>
2012-01-11 11:12:22 +0000

    This commit was manufactured by cvs2git to create tag 'RecoEgamma- EgammaPhotonAlgos-V04-04-02-01'.
    Sprout from master 2011-11-02 19:03:44 UTC Nancy Marinelli <sha1-ef5da52cfaf56298cecd1a4c085ed43aaf8901ee@cern.ch> 'BuildFile.xml'
    Cherrypick from master 2012-01-11 11:12:20 UTC Nancy Marinelli <sha1-ef5da52cfaf56298cecd1a4c085ed43aaf8901ee@cern.ch> 'Access rec hit collection by  input tag':
        interface/PhotonEnergyCorrector.h
        src/PhotonEnergyCorrector.cc

--- src/PhotonEnergyCorrector.cc
+++ src/PhotonEnergyCorrector.cc
@@ -1,46 +1,52 @@
 #include "RecoEgamma/EgammaPhotonAlgos/interface/PhotonEnergyCorrector.h"
+#include "RecoEcal/EgammaCoreTools/interface/EcalClusterLazyTools.h"
 
 
-PhotonEnergyCorrector::PhotonEnergyCorrector( const edm::ParameterSet& config, const edm::EventSetup& theEventSetup  ) {
+PhotonEnergyCorrector::PhotonEnergyCorrector( const edm::ParameterSet& config ) {
 
 
   minR9Barrel_        = config.getParameter<double>("minR9Barrel");
   minR9Endcap_        = config.getParameter<double>("minR9Endcap");
   // get the geometry from the event setup:
-  theEventSetup.get<CaloGeometryRecord>().get(theCaloGeom_);
-  candidateP4type_ = config.getParameter<std::string>("candidateP4type") ;
+
+  barrelEcalHits_   = config.getParameter<edm::InputTag>("barrelEcalHits");
+  endcapEcalHits_   = config.getParameter<edm::InputTag>("endcapEcalHits");
+  //  candidateP4type_ = config.getParameter<std::string>("candidateP4type") ;
 
 
   // function to extract f(eta) correction
   scEnergyFunction_ = 0 ;
   std::string superClusterFunctionName = config.getParameter<std::string>("superClusterEnergyCorrFunction") ;
   scEnergyFunction_ = EcalClusterFunctionFactory::get()->create(superClusterFunctionName,config) ;
-  scEnergyFunction_->init(theEventSetup); 
+
 
   // function to extract corrections to cracks
   scCrackEnergyFunction_ = 0 ;
   std::string superClusterCrackFunctionName = config.getParameter<std::string>("superClusterCrackEnergyCorrFunction") ;
   scCrackEnergyFunction_ = EcalClusterFunctionFactory::get()->create(superClusterCrackFunctionName,config) ;
-  scCrackEnergyFunction_->init(theEventSetup); 
+
 
   // function to extract the error on the sc ecal correction
   scEnergyErrorFunction_ = 0 ;
   std::string superClusterErrorFunctionName = config.getParameter<std::string>("superClusterEnergyErrorFunction") ;
   scEnergyErrorFunction_ = EcalClusterFunctionFactory::get()->create(superClusterErrorFunctionName,config) ;
-  scEnergyErrorFunction_->init(theEventSetup); 
+
 
   // function  to extract the error on the photon ecal correction
   photonEcalEnergyCorrFunction_=0;
   std::string photonEnergyFunctionName = config.getParameter<std::string>("photonEcalEnergyCorrFunction") ;
   photonEcalEnergyCorrFunction_ = EcalClusterFunctionFactory::get()->create(photonEnergyFunctionName, config);
-  photonEcalEnergyCorrFunction_->init(theEventSetup);
 
 
-  // ingredient for energy regression  
-  w_file_ = config.getParameter<std::string>("energyRegressionWeightsLocation");
+
+  // ingredient for energy regression
+  weightsfromDB_= config.getParameter<bool>("regressionWeightsFromDB");
+  w_file_ = config.getParameter<std::string>("energyRegressionWeightsFileLocation");
+  if (weightsfromDB_) w_db_   = config.getParameter<std::string>("energyRegressionWeightsDBLocation");
+  else  w_db_ == "none" ;
   regressionCorrector_ = new EGEnergyCorrector(); 
-  if ( ! (w_file_ == "none") ) 
-    if (!regressionCorrector_->IsInitialized()) regressionCorrector_->Initialize(theEventSetup,w_file_);
+
+
 }
 
 
@@ -50,7 +56,26 @@ PhotonEnergyCorrector::~PhotonEnergyCorrector() {
 
 
 
-void PhotonEnergyCorrector::calculate(reco::Photon & thePhoton, int subdet) {
+void PhotonEnergyCorrector::init (  const edm::EventSetup& theEventSetup ) {
+  theEventSetup.get<CaloGeometryRecord>().get(theCaloGeom_);
+
+  scEnergyFunction_->init(theEventSetup); 
+  scCrackEnergyFunction_->init(theEventSetup); 
+  scEnergyErrorFunction_->init(theEventSetup); 
+  photonEcalEnergyCorrFunction_->init(theEventSetup);
+
+  if ( weightsfromDB_ ) {
+    if (!regressionCorrector_->IsInitialized()) regressionCorrector_->Initialize(theEventSetup,w_db_,weightsfromDB_);
+  }
+  if ( !weightsfromDB_ &&  !(w_file_ == "none")  ) {
+    if (!regressionCorrector_->IsInitialized()) regressionCorrector_->Initialize(theEventSetup,w_file_,weightsfromDB_);
+  }  
+
+
+}
+
+
+void PhotonEnergyCorrector::calculate(edm::Event& evt, reco::Photon & thePhoton, int subdet, const reco::VertexCollection& vtxcol, const edm::EventSetup& iSetup  ) {
   
   double phoEcalEnergy = -9999.;
   double phoEcalEnergyError = -9999.;
@@ -58,13 +83,6 @@ void PhotonEnergyCorrector::calculate(reco::Photon & thePhoton, int subdet) {
   double phoRegr1EnergyError = -9999.;
   const CaloSubdetectorGeometry* subDetGeometry =0 ;
   subDetGeometry =  theCaloGeom_->getSubdetectorGeometry(DetId::Ecal, subdet);
-  bool p4FromEcal=false;
-  bool p4FromRegression=false;
-  if ( candidateP4type_ == "fromEcalEnergy") {
-    p4FromEcal=true;
-  } else if ( candidateP4type_ == "fromRegression" &&   !(w_file_ == "none")  ) {
-    p4FromRegression=true;
-  }
 
   double minR9=0;
   if (subdet==EcalBarrel) {
@@ -73,6 +91,23 @@ void PhotonEnergyCorrector::calculate(reco::Photon & thePhoton, int subdet) {
     minR9=minR9Endcap_;
   }
 
+ 
+
+  EcalClusterLazyTools lazyTools(evt, iSetup, barrelEcalHits_,endcapEcalHits_);  
+
+
+  ////////////// Here default Ecal corrections based on electrons  ////////////////////////
+  if ( thePhoton.r9() > minR9 ) {
+    // f(eta) correction to e5x5
+    double deltaE = scEnergyFunction_->getValue(*(thePhoton.superCluster()), 1);
+    float e5x5=thePhoton.e5x5();
+    if (subdet==EcalBarrel) e5x5 = e5x5 * (1.0 +  deltaE/thePhoton.superCluster()->rawEnergy() );
+    phoEcalEnergy =  e5x5    +  thePhoton.superCluster()->preshowerEnergy() ;  
+  } else {
+    phoEcalEnergy = thePhoton.superCluster()->energy();
+  }
+  // store the value in the Photon.h
+  thePhoton.setCorrectedEnergy( reco::Photon::ecal_standard, phoEcalEnergy, phoEcalEnergyError,  false);
 
   ////////////// Here Ecal corrections specific for photons ////////////////////////
   // correction for low r9 
@@ -90,19 +125,20 @@ void PhotonEnergyCorrector::calculate(reco::Photon & thePhoton, int subdet) {
   phoEcalEnergy *=  scCrackEnergyFunction_->getValue(*(thePhoton.superCluster()));
   // as for the erros use the error on the SC 
   phoEcalEnergyError =   scEnergyErrorFunction_->getValue(*(thePhoton.superCluster()), 0);  
-  // set the value in the Photon.h
-  //  thePhoton.setCorrectedEnergy( reco::Photon::ecal_photons, phoEcalEnergy, phoEcalEnergyError,  p4FromEcal);
+  // store the value in the Photon.h
   thePhoton.setCorrectedEnergy( reco::Photon::ecal_photons, phoEcalEnergy, phoEcalEnergyError,  false);
 
   //////////  Energy  Regression ////////////////////// 
-  if ( !(w_file_ == "none") ) {
-    std::pair<double,double> cor = regressionCorrector_->CorrectedEnergyWithError(thePhoton);
+  //
+  if ( weightsfromDB_  || ( !weightsfromDB_ && !(w_file_ == "none") ) ) {
+    std::pair<double,double> cor = regressionCorrector_->CorrectedEnergyWithError(thePhoton, vtxcol, lazyTools, iSetup);
     phoRegr1Energy = cor.first;
     phoRegr1EnergyError = cor.second;
-    // set the value in the Photon.h
-    // thePhoton.setCorrectedEnergy( reco::Photon::regression1, phoRegr1Energy, phoRegr1EnergyError,  p4FromRegression);
+    // store the value in the Photon.h
     thePhoton.setCorrectedEnergy( reco::Photon::regression1, phoRegr1Energy, phoRegr1EnergyError,  false);
-  }
+  } 
+
+
 
   /*
   std::cout << " ------------------------- " << std::endl;
